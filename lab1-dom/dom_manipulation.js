370eb97fc08fc9c689f941c5ab1e24fb42e61c9c
Riccardo Crepaldi <crepric@google.com>
2019-06-15 03:42:21 -0700

    Simplifies solution, adds more tasks to the lab.

--- lab1-dom/dom_manipulation.js
+++ lab1-dom/dom_manipulation.js
@@ -2,6 +2,16 @@
 let pageSetup = () => {
   console.log('setting up page');
   document.getElementById('sort_btn').addEventListener('click', sortSongs);
+  document.getElementById('add_btn').addEventListener('click', addSong);
+  document.getElementById('clear_btn').addEventListener('click', clearList);
+}
+
+// Clear the list, removing every song in it.
+let clearList = () => {
+}
+
+// Add a new song based on the fields entered by the user.
+let addSong = () => {
 }
 
 // Sorts songs based on seletor value.
--- lab1-dom/dom_manipulation_solution.js
+++ lab1-dom/dom_manipulation_solution.js
@@ -21,6 +21,35 @@ const sorting_params = {
 let pageSetup = () => {
   console.log('setting up page');
   document.getElementById('sort_btn').addEventListener('click', sortSongs);
+  document.getElementById('add_btn').addEventListener('click', addSong);
+  document.getElementById('clear_btn').addEventListener('click', clearList);
+}
+
+// Removes all elements from the LI
+let clearList = () => {
+  document.querySelector('#billboard').innerHTML = '';
+}
+
+// This is not the best way to build an HTML element in JS, but given the small
+// size of it, we can create an HTML string. Ideally, we would use
+// createElement() and appendChild() as demonstrated below.
+let buildListItemHtml = (pos, artist, song) => {
+  return '<span class="billboard_pos">' + pos + '</span> - ' +
+         '<span class="artist">' + artist + '</span> - ' +
+         '<span class="song">' + song + '</span>';
+}
+
+let addSong = () => {
+  // Create a new list item using data from the form.
+  let song_title = document.querySelector("#new_song_title").value;
+  let song_position = document.querySelector("#new_song_pos").value;
+  let song_artist= document.querySelector("#new_song_artist").value;
+  let new_song_list_item = document.createElement("li");
+  new_song_list_item.innerHTML =
+      buildListItemHtml(song_position, song_artist, song_title);
+  // Attach the new element to the existing list;
+  let dom_list_element = document.querySelector('#billboard');
+  dom_list_element.appendChild(new_song_list_item);
 }
 
 // Sorts songs based on seletor value.
@@ -34,11 +63,14 @@ let sortSongs = (e) => {
   // Based on the selection by the user, let's find the sorting parameters.
   let sorting_criterion =
     document.querySelector('#sort_selector').selectedOptions[0].value;
+  let sorting_direction_inverse =
+    document.querySelector('#chk_inverse').checked;
   // Array.sort() sorts the aray in place, but in our case it needs a dedicated
   // sorting function, since we're not simply sorting the <li> items
   // alphabeticallly
   songs_array.sort((first, second) => {
-    return compareSongs(first, second, sorting_params[sorting_criterion]);
+    return compareSongs(first, second, sorting_params[sorting_criterion],
+                        sorting_direction_inverse);
   });
   clearList();
   rebuildList(songs_array);
@@ -47,57 +79,29 @@ let sortSongs = (e) => {
   document.querySelector('#sorted_desc').innerText = sorted_description;
 }
 
-// Removes all elements from the LI
-let clearList = () => {
-  document.querySelector('#billboard').innerHTML = '';
-}
-
-// This is not the best way to build an HTML element in JS, but given the small
-// size of it, we can create an HTML string. Ideally, we would use
-// createElement() and appendChild() as demonstrated below.
-let buildListItemHtml = (pos, artist, song) => {
-  return '<span class="billboard_pos">' + pos + '</span> - ' + 
-         '<span class="artist">' + artist + '</span> - ' + 
-         '<span class="song">' + song + '</span>';
-}
-
 // After clearing the list, we use the sorted array to rebuild it in the right
 // order.
 let rebuildList = (new_list) => {
-  let list = document.querySelector('#billboard');
+  let dom_list_element = document.querySelector('#billboard');
   new_list.forEach((element) => {
-    // Extract the fields for the song.
-    let song_pos =
-      element.querySelector(sorting_params['by_pos'].selector).innerText;
-    let artist =
-      element.querySelector(sorting_params['by_artist'].selector).innerText;
-    let song =
-      element.querySelector(sorting_params['by_song'].selector).innerText;
-    // Create a new LI item.
-    let new_item =
-      document.createElement('li');
-    // Fill the HTML inside the LI element.
-    new_item.innerHTML =
-      buildListItemHtml(song_pos, artist, song);
-    // Add the element to the List.
-    list.appendChild(new_item);
+    dom_list_element.appendChild(element);
   });
 }
 
 // This is the function that sort() uses to compare songs based on the selected
 // criterion.
-let compareSongs = (first, second, criterion) => {
+let compareSongs = (first, second, criterion, inverse) => {
   let first_field = first.querySelector(criterion.selector).innerText;
   let second_field = second.querySelector(criterion.selector).innerText;
   if (criterion.numeric) {
-    return first_field - second_field;
+    return (first_field - second_field) * (inverse ? -1 : 1);
   }
   if (first_field === second_field) {
     return 0;
   } else if (first_field < second_field) {
-    return -1;
+    return inverse ? 1 : -1;
   } else {
-    return 1;
+    return inverse ? -1 : 1;
   }
 }
 
